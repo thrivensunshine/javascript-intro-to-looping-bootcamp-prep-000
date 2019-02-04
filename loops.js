b9b794fee85da7cf6374032a8ec0ea74e238934a
DANIELLE ALBA <danib.sgh@gmail.com>
2019-02-04 03:23:08 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -17,3 +17,15 @@ function whileLoop(num){
   }
 }
 
+var i = 0
+function doWhileLoop(num) {
+  function incrementVariable() {
+  i = i + 1;
+  return i
+}
+
+ 
+do {
+  console.log("I run once regardless");
+} while (incrementVariable() < num);
+}
